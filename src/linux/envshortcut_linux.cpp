418167139b3938a023bd07c2d04ae956c04063b3
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-03-17 21:54:50 +0100

    the linux implementation of Shortcut::add now extracts the icon of pe files

--- src/linux/envshortcut_linux.cpp
+++ src/linux/envshortcut_linux.cpp
@@ -1,24 +1,61 @@
 #include "envshortcut.h"
-#include <QtDBus/QtDBus>
 #include <log.h>
+#include <uibase/peextractor.h>
 #include <utility.h>
 
 namespace env
 {
 
 using namespace MOBase;
+using namespace Qt::StringLiterals;
+
+inline QString iconPath(int resolution)
+{
+  QString res = QString::number(resolution);
+  return QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation)
+             .first() %
+         u"/icons/hicolor/"_s % res % u"x"_s % res % u"/apps/"_s;
+}
 
 bool Shortcut::add(Locations loc)
 {
+  // extract icon from PE file
+  if (m_icon.endsWith("exe"_L1, Qt::CaseInsensitive)) {
+    // open file
+    QFile file(m_icon);
+    if (file.open(QIODeviceBase::ReadOnly)) {
+      // extract data
+      QBuffer buff;
+      if (buff.open(QIODeviceBase::ReadWrite)) {
+        if (PeExtractor::loadIconData(&file, &buff)) {
+          QImage image = QImage::fromData(buff.data());
+          m_icon = iconPath(image.size().width()) % u"mo2-"_s % m_name % u".png"_s;
+          // save icon to disk
+          if (!image.save(m_icon)) {
+            log::warn("error saving icon file {}", m_icon);
+          } else {
+            log::debug("created icon {}", m_icon);
+          }
+        } else {
+          log::warn("error in loadIconData");
+        }
+      } else {
+        log::warn("error creating buffer for icon extraction");
+      }
+    } else {
+      log::warn("error opening {} for icon extraction, {}", m_icon, file.errorString());
+    }
+  }
+
   log::debug("adding shortcut to {}:\n"
              "  . name: '{}'\n"
              "  . target: '{}'\n"
              "  . arguments: '{}'\n"
              "  . description: '{}'\n"
-             "  . icon: '{}' @ {}\n"
+             "  . icon: '{}'\n"
              "  . working directory: '{}'",
              toString(loc), m_name, m_target, m_arguments, m_description, m_icon,
-             m_iconIndex, m_workingDirectory);
+             m_workingDirectory);
 
   if (m_target.isEmpty()) {
     log::error("shortcut: target is empty");
@@ -42,7 +79,7 @@ bool Shortcut::add(Locations loc)
   log::debug("shortcut file will be saved at '{}'", path);
 
   QString fileContent =
-      QString("#!/usr/bin/env xdg-open\n"
+      QString("#!/usr/bin/env xdg-open\n\n"
               "[Desktop Entry]\n"
               "Name=%1\n"
               "Exec=%2 %3\n"
@@ -66,6 +103,7 @@ bool Shortcut::add(Locations loc)
 
 bool Shortcut::remove(Locations loc)
 {
+  // todo: clean up orphaned icon files
   log::debug("removing shortcut for '{}' from {}", m_name, toString(loc));
 
   const auto path = shortcutPath(loc);
@@ -82,9 +120,7 @@ bool Shortcut::remove(Locations loc)
 
   if (!QFile::remove(path)) {
     const auto e = errno;
-
     log::error("failed to remove shortcut '{}', {}", path, strerror(e));
-
     return false;
   }
 
