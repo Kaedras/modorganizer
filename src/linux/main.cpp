b42687f3d65093ff7d149ed4791d2b56916206b3
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-04-03 22:31:30 +0200

    core dumps are now handled by the kernel

--- src/linux/main.cpp
+++ src/linux/main.cpp
@@ -10,8 +10,14 @@
 #include "thread_utils.h"
 #include <log.h>
 #include <report.h>
+#include <sys/resource.h>
 
 using namespace MOBase;
+
+// 1 GiB limit, compressed size will be much lower
+static constexpr rlim_t coreDumpSizeLimit = 1024 * 1024 * 1024;
+// anonymous private mappings
+static constexpr int coreDumpFilter = 0b000'0001;
 
 thread_local std::terminate_handler g_prevTerminateHandler = nullptr;
 
@@ -19,6 +25,30 @@ int run(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
+  // let the kernel handle core dumps for now
+
+  // set core dump limit to enable crash dumps
+  // currently the produced core dump will be ~600 MiB, bz2 compression reduces the size
+  // to ~15 MiB
+  rlimit core_limits{coreDumpSizeLimit, coreDumpSizeLimit};
+  setrlimit(RLIMIT_CORE, &core_limits);
+
+  // set core dump filter
+  std::ofstream filter("/proc/self/coredump_filter");
+  if (filter.is_open()) {
+    filter << coreDumpFilter << std::endl;
+    filter.close();
+  } else {
+    const int e = errno;
+    log::warn("Error writing coredump_filter, {}. Kernel may not be built with "
+              "CONFIG_ELF_CORE.",
+              strerror(e));
+  }
+
+  // todo: clean up old crash dumps, move them into crashDumps folder
+
+  // todo: test with systemd (coredumpctl)
+
   const int r = run(argc, argv);
   std::cout << "mod organizer done\n";
   return r;
@@ -28,6 +58,7 @@ int run(int argc, char* argv[])
 {
   MOShared::SetThisThreadName("main");
   setExceptionHandlers();
+  // setExceptionHandlers();
 
   cl::CommandLine cl;
 
