11a766e66d54fcc6922be1f94cfb5281a4935cff
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-02-13 14:39:43 +0100

    Disable mount namespaces for now as is causes issues in appimages

--- src/linux/usvfsconnector.cpp
+++ src/linux/usvfsconnector.cpp
@@ -72,7 +72,7 @@ UsvfsConnector::UsvfsConnector() : m_usvfsManager(UsvfsManager::instance())
   const LogLevel logLevel = toUsvfsLogLevel(s.diagnostics().logLevel());
   const auto delay        = duration_cast<milliseconds>(s.diagnostics().spawnDelay());
 
-  m_usvfsManager->setUseMountNamespace(true);
+  // m_usvfsManager->setUseMountNamespace(true);
 
   const QString logFileName =
       qApp->property("dataPath").toString() % QStringLiteral("/logs/usvfs.log");
